f3c64c07eb7dfa32cfa1274104c16ffa81f1992e
Iain MacDonald <IJMacD@gmail.com>
2014-05-27 18:34:10 +0800

    Added javascript version

---
+++ js/genex.js
@@ -0,0 +1,110 @@
+$(function(){
+	var uploadBtn = $('#btn-upload'),
+		importFile = $('#import-file'),
+		dropTarget = $('.drop-target'),
+		enexTemplate = $('#tmpl-enex').html(),
+		enexNoteTemplate = $('#tmpl-enex-note').html(),
+		linkList;
+	
+	uploadBtn.on("click", function(e){
+		importFile.click();
+	});
+	
+	importFile.on("change", function(e){
+		var files = e.target.files;
+		processFiles(files);
+	});
+
+	dropTarget.on("dragover", function(e){
+		e.preventDefault();
+		return false;
+	});
+
+	dropTarget.on("dragenter", function(e){
+		$(e.target).addClass("drag-over");
+	});
+
+	dropTarget.on("dragleave", function(e){
+		$(e.target).removeClass("drag-over");
+	});
+
+	dropTarget.on("drop", function(e){
+		e.stopPropagation();
+		e.preventDefault();
+
+		var files = e.originalEvent.dataTransfer.files;
+		
+		processFiles(files);
+
+		return false;
+	});
+
+	dropTarget.on("dragend", function(e){
+		$(e.target).removeClass("drag-over");
+	});
+	
+	function processFiles(files){
+		if(!files || !files[0]){
+			return
+		}
+
+		var file = files && files[0];
+		parseFile(file).then(function(html){
+			var bookmarks = parseHTML(html),
+				enex = generateEnex(bookmarks),
+				filename = file.name+".enex",
+				outFile = generateDataURL(enex, filename);
+			
+			addDownloadLink(outFile, filename);
+		}).catch(function(e){
+			console.error(e.stack);
+		});
+	}
+	
+	function parseFile(file){
+		var fileReader = new FileReader();
+		return new Promise(function(resolve, reject){
+			fileReader.onload = function(e){
+				resolve(e.target.result);
+			};
+		
+			fileReader.readAsText(file);
+		});
+	}
+	
+	function parseHTML(html){
+		var doc = $(html),
+			bookmarks = doc.find("a").map(function(i,a){
+				var $a = $(a);
+				return {url: $a.attr("href"), title: $a.text(), created: new Date($a.attr("ADD_DATE"))};
+			});
+		return bookmarks;
+	}
+	
+	function generateEnex(bookmarks){
+		var doc = $($.parseXML(riot.render(enexTemplate, {created: new Date()}))),
+			elem = doc.find("en-export");
+		bookmarks.each(function(i, note){
+			var fragment = riot.render(enexNoteTemplate, note);
+			try {
+				elem.append(fragment);
+			} catch (e){
+				console.log(fragment);
+			}
+		});
+		return doc[0].documentElement.outerHTML;
+	}
+	
+	function generateDataURL(enex){
+		return "data:,"+encodeURIComponent(enex);
+	}
+	
+	function addDownloadLink(link, title){
+		if(!linkList){
+			linkList = $('<ul>').appendTo('.container');
+		}
+		var item = $('<li><a href="'+link+'" download="'+title+'">'+(title||link)+'</a>');
+		item.find("a")[0].download = title;
+		linkList.append(item);
+	}
+});
